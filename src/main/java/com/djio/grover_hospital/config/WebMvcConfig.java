a81c77da015fb5d5d6f63606bdedeca2d0003ea6
David-Jesse <djesse351@gmail.com>
2026-04-30 15:28:24 +0100

    added the FileStorageException file and the WebMvcConfig file

---
+++ src/main/java/com/djio/grover_hospital/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.djio.grover_hospital.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${app.file-storage.upload-dir:./uploads/images}")
+    private String uploadDir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        Path uploadPath = Paths.get(uploadDir).toAbsolutePath().normalize();
+        String resourceLocation = "file:" + uploadPath.toString() + "/";
+
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations(resourceLocation)
+                .setCachePeriod(3600);
+    }
+}
--- src/main/java/com/djio/grover_hospital/exception/FileStorageException.java
+++ src/main/java/com/djio/grover_hospital/exception/FileStorageException.java
@@ -1,4 +1,17 @@
 package com.djio.grover_hospital.exception;
 
-public class FileStorageException {
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class FileStorageException extends RuntimeException {
+
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
